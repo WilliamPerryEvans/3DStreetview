4b67f1ab29feff33502c2b097b2446de046ad6ca
hcwinsemius <h.c.winsemius@gmail.com>
2021-08-17 12:29:57 +0200

    Upload status bar implemented #30

--- static/js/mesh/odk2odm.js
+++ static/js/mesh/odk2odm.js
@@ -2,8 +2,10 @@ const odkconfig_id = $('input#odk_config').val()
 const odkproject_id = $('input#odkproject_id').val();
 const odmconfig_id = $('input#odm_config').val();
 const odmproject_id = $('input#odmproject_id').val();
-
 const id = $('input#mesh_id').val();
+var progress_title = "Upload progress";
+var odk2odm_total = 0;
+var odk2odm_progress = 0;  // percentage of upload progress
 
 $(document).ready(function () {
     // fill drop down
@@ -83,7 +85,6 @@ function get_odm_tasks()
                 $.getJSON(
                     `/api/odm/${odmconfig_id}/projects/${odmproject_id}/tasks/${x}`,
                     function(task) {
-//                    });
                         var option = document.createElement("option");
                         option.text = `Name: ${task.name} id: ${task.id}`;
                         option.value = task.id;
@@ -93,8 +94,6 @@ function get_odm_tasks()
             flashMessage([{"type": "success", "message": "Retrieved tasks"}]);
             document.getElementById("task_create_button").disabled = false;
             document.getElementById("task_delete_button").disabled = false;
-//            // fill information about the current selected task
-//            get_odm_task();
         }
     )
     .fail(function() {
@@ -108,23 +107,26 @@ function get_odm_task()
     // get current id of mesh
     var task_select = document.getElementById("odm_task");
     const task_id = task_select.value;
-    document.getElementById("task_create_button").disabled = true;
-    document.getElementById("task_delete_button").disabled = true;
-    // clear current tasks in dropdown
-    $.getJSON(
-        `/api/odm/${odmconfig_id}/projects/${odmproject_id}/tasks/${task_id}`,
-        function(data) {
+    if (task_id != "null") {
+        document.getElementById("task_create_button").disabled = true;
+        document.getElementById("task_delete_button").disabled = true;
+        // clear current tasks in dropdown
+        $.getJSON(
+            `/api/odm/${odmconfig_id}/projects/${odmproject_id}/tasks/${task_id}`,
+            function(data) {
                 // change content of status texts and bars
                 $('#task_id span').text(`Task: ${task_id}`);
                 $('#images_count span').text(` ${data.images_count}`);
                 $('#processing_time span').text(` ${millisToMinutesAndSeconds(data.processing_time)}`);
+                $('#progress_title').text(progress_title);
                 prog = document.getElementById('upload_progress')
-                prog.style = `width: ${data.upload_progress*100}%`
-                prog.textContent = `${Math.round(data.upload_progress*data.images_count)}/${data.images_count}`;
+                prog.style = `width: ${odk2odm_progress/odk2odm_total*100}%`
+                prog.textContent = `${odk2odm_progress}/${odk2odm_total}`;
+//                prog.textContent = `${Math.round(data.upload_progress*data.images_count)}/${data.images_count}`;
                 prog = document.getElementById('running_progress')
                 prog.style = `width: ${data.running_progress*100}%`
                 prog.textContent = `${Math.round(data.running_progress*100)} %`;
-                console.log(data);
+//                console.log(data);
                 // handle status of buttons
                 if (data.images_count > 0 && (data.status != 10 && data.status != 20)){   // status checks are according to https://docs.webodm.org/#status-codes
                     // make the run button active
@@ -135,10 +137,12 @@ function get_odm_task()
                     document.getElementById('odm_cancel').disabled = false
                     document.getElementById('odm_delete').disabled = false
                 }
-            });
-            flashMessage([{"type": "success", "message": `Retrieved task ${task_id}`}]);
-            document.getElementById("task_create_button").disabled = false;
-            document.getElementById("task_delete_button").disabled = false;
+            }
+        );
+        flashMessage([{"type": "success", "message": `Retrieved task ${task_id}`}]);
+        document.getElementById("task_create_button").disabled = false;
+        document.getElementById("task_delete_button").disabled = false;
+    }
 }
 
 
@@ -172,6 +176,7 @@ function create_odm_task() {
 }
 
 function getodk_postodm(all=false) {
+    odk2odm_progress = 0;
 //    alert('This button will allow you to transfer. Not implemented yet.')
     // get the selected items
     if (all==false) {
@@ -181,6 +186,11 @@ function getodk_postodm(all=false) {
         // get all items
         submissions = $('#submissions').bootstrapTable('getData');
     }
+    // first get all attachment we want
+    attachments = submissions.map(get_attachments);
+    no_attachments = attachments.map(a => a.length);
+    odk2odm_total = no_attachments.reduce((a, b) => a + b, 0)  // sum up all lengths of attachments to know total no. photos
+//    console.log(no_attachments);
 //    console.log(submissions);
     // get a list (per survey) of lists of all attachments in each survey
     // check if an ODM task is selected, and has the right status if not return msg
@@ -248,7 +258,9 @@ function file_transfer(attachment, instanceId){
             success: function(data) {
                 console.log(data);
                 // refresh project list
-                flashMessage([{"type": "success", "message": `Uploaded file ${attachment.name}`}]);
+                odk2odm_progress += 1;
+                get_odm_task();
+                progress_title = `Uploaded ${attachment.name}`;
 
             },
             // Enable save button again.
@@ -285,3 +297,31 @@ function buttons () {
     }
 }
 
+//function update_odm() {
+//    console.log("Updating ODM task status");
+//    task_id: document.getElementById("odm_task").value
+//    if (task_id != "null") {
+//        // check if the task can be retrieved
+//        $.getJSON(
+//            `/api/odm/${odmconfig_id}/projects/${odmproject_id}/tasks/${task_id}`,
+//            function(data) {
+//                if ("id" in data) {
+//                    // check if status is open to photos
+//
+//                    if (data.status != 10 && data.status != 20) { // not queued or running
+//                        // push all photos to odm server
+//                        submissions.map(t => transfer(t, ));
+//                    }
+//                } else {
+//                    console.log(`Cannot update task ${task_id}`);
+//                }
+//            }
+//        )
+//        .fail(function() {
+//            // flash a message as the API call failed
+//            flashMessage([{"type": "danger", "message": "Not able to retrieve task, is the ODM server offline?"}]);
+//        });
+//
+//
+//}
+//setInterval('get_odm_task()', 1000);
