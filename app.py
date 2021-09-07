69c523275dc017b1e57ade6ef9cd2d6476e4563b
hcwinsemius <h.c.winsemius@gmail.com>
2021-09-07 17:43:07 +0200

    provisioning of mail variables in .env #53

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ def create_app(config_name):
     app.config["FERNET_KEY"] = os.getenv("FERNET_KEY")
     app.config["CELERY_BROKER_URL"] = os.getenv("CELERY_URL")
     app.config["result_backend"] = os.getenv("CELERY_URL")
-    app.config["MAIL_SERVER"] = "smtp.gmail.com"
-    app.config["MAIL_PORT"] = 587
-    app.config["MAIL_USE_TLS"] = True
+    app.config["MAIL_SERVER"] = os.getenv("MAIL_SERVER")
+    app.config["MAIL_PORT"] = int(os.getenv("MAIL_PORT"))
+    app.config["MAIL_USE_TLS"] = bool(os.getenv("MAIL_USE_TLS"))
     app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
     app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
     app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_SENDER")
