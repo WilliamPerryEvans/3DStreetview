cc625ecd00c5f5608822ecb33b73ed3b14c72f7a
hcwinsemius <h.c.winsemius@gmail.com>
2021-10-01 13:37:04 +0200

    moved application to package #57

--- streetview/models/__init__.py
+++ streetview/models/__init__.py
@@ -4,15 +4,21 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from streetview.models.base import Base
 
+
 postgres_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_SERVER')}:{os.getenv('POSTGRES_PORT')}/{os.getenv('POSTGRES_DB')}"
 engine = create_engine(postgres_url, pool_size=50, pool_recycle=3600, pool_pre_ping=0, max_overflow=0)
 
+from streetview.models import user
 from streetview.models import odk
+from streetview.models import odm
+from streetview.models import odmproject
+from streetview.models import odkproject
+from streetview.models import mesh
+from streetview.models import game
 
 # TODO: Persistent database by removing drop all once DB models are stable..
 # Base.metadata.drop_all(engine)
-
-Base.metadata.create_all(engine)
+# Base.metadata.create_all(engine)
 DBSession = sessionmaker(bind=engine)
 db = scoped_session(DBSession)
 Base.query = db.query_property()
